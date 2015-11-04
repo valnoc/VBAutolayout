3cf76e0cd647435f107228e1036be6019e3e7e90
Valeriy Bezuglyy <valnocorner@gmail.com>
2015-11-04 18:48:07 +0300

    regexp

--- Example/VBAutolayoutExample/ViewController.m
+++ Example/VBAutolayoutExample/ViewController.m
@@ -17,6 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString* value = @">=10@999";
+    value = @"10@999";
+    value = @"10";
+
+    NSArray* comps = [[value stringByReplacingOccurrencesOfString:@"(\\D*)(\\d*)@?(\\d*)"
+                                                      withString:@"$1,$2,$3"
+                                                         options:NSRegularExpressionSearch
+                                                           range:NSMakeRange(0, value.length)]
+                      componentsSeparatedByString:@","];
+    NSLog(@"%@ = %@", value, comps);
 }
 
 - (void)didReceiveMemoryWarning {
